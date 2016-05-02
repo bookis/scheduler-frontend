a9085629b1ff475d218c9b75b8f1413d83c63aeb
LeShoyaStarkey <olstarkey@gmail.com>
2016-05-02 14:29:44 -0700

    added additional styling to send form after toggle

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ $( document ).ready(function() {
   console.log('hi');
   $('body').on('click', '#schedule', function(){
     console.log("hello");
-    $('.schedule-send').removeClass('schedule-send');
+    $('.schedule-send').toggle();
+    $('.email-create').toggle();
+    // (use toggle versus add class)
+      // $('.schedule-send').removeClass('schedule-send');
       return false;
   })
 });
