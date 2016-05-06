3b9b5490b7080e209c9388f68538d0151767ab1a
LeShoyaStarkey <olstarkey@gmail.com>
2016-05-06 14:20:35 -0700

    restyled form with basic rounded corners and fixed functioning of datepicker with new jquery

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 $( document ).ready(function() {
 
-  $('body').on('click', '#schedule', function(){
-    // (use remove class versus toggle for schedule send)
-    $('.schedule-send').removeClass('schedule-send');
-    $('.email-create').toggle();
+  $('body').on('click', '#datepicker', function(){
+  //   // (use remove class versus toggle for schedule send)
+  //   $('.schedule-send').removeClass('schedule-send');
+  //   $('.email-create').toggle();
     var picker = new Pikaday({ field: $('#datepicker')[0], reposition:false });
     return false;
-  })
-
-  $('body').on('click', '.go-back', function(){
-      parent.history.back();
-      return false;
+  // })
+  //
+  // $('body').on('click', '.go-back', function(){
+  //     parent.history.back();
+  //     return false;
   })
 
 });
