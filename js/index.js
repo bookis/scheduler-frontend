7558ef6b6708ea2eafd578e57ab06ef76d4590d6
LeShoyaStarkey <olstarkey@gmail.com>
2016-05-13 12:03:35 -0700

    cleaned up js index js file

--- js/index.js
+++ js/index.js
@@ -3,12 +3,8 @@ $( document ).ready(function() {
   $('#datepicker').attr("placeholder", Date());
 
   $('body').on('click', '#datepicker', function(){
-  //   // (use remove class versus toggle for schedule send)
-  //   $('.schedule-send').removeClass('schedule-send');
-  //   $('.email-create').toggle();
     var picker = new Pikaday({ field: $('#datepicker')[0], reposition:false });
     return false;
-
   })
 
   $('body').on('click', '#plus', function(){
@@ -17,7 +13,6 @@ $( document ).ready(function() {
     })
 
   $('body').on('click', '.outbox', function(){
-      console.log("help")
       $('.blue-box').hide();
       $('.email-form').hide();
       $('.emails-container').toggle();
